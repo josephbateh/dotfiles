fcd4a8ae382c42c97117fe38f6b3e396ed1af211
Joseph Bateh <josephbateh@gmail.com>
2017-06-25 15:40:12 -0400

    Microsoft Remote Desktop

--- install/mas-cli.sh
+++ install/mas-cli.sh
@@ -1,11 +1,15 @@
 # Install mas
 brew install mas
 
+# Search apps
+mas search appname
+
 # Slack: 803453959
 # Magnet: 441258766
 # XCode: 497799835
 # Pages: 409201541
 # iMovie: 408981434
+# Microsoft Remote Desktop: 715768417
 
 # Install packages
 apps=(
@@ -14,6 +18,7 @@ apps=(
   497799835
   409201541
   408981434
+  715768417
 )
 
 mas install "${apps[@]}"
