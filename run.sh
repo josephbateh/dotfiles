f849202b344d792e7e75202ee4e721f2bbe45805
Joseph Bateh <github@josephbateh.com>
2020-01-25 10:16:07 -0500

    Convert to one script

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-# Run Setup Scripts
-zsh scripts/install.sh
-zsh scripts/settings.sh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,60 @@
 #!/usr/bin/env zsh
-
-# Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Taps
+brew tap mongodb/brew
+brew tap linode/cli
+
+# Install utilities
+brew install awscli
+brew install dockutil
+brew install dotnet
+brew install git
+brew install glances
+brew install htop
+brew install linode-cli
+brew install mas
+brew install mongodb-community
+brew install mono
+brew install watch
+
+# Install Casks
+brew cask install aerial
+brew cask install alfred
+brew cask install atom
+brew cask install cyberduck
+brew cask install datagrip
+brew cask install discord
+brew cask install docker
+brew cask install firefox
+brew cask install gitkraken
+brew cask install google-chrome
+brew cask install intel-power-gadget
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install lastpass
+brew cask install nextcloud
+brew cask install rider
+brew cask install sensiblesidebuttons
+brew cask install spotify
+brew cask install webstorm
+brew cask install visual-studio-code
+
+# Install Mac App Store Apps
+mas install 803453959    # Slack
+mas install 441258766    # Magnet
+mas install 409201541    # Pages
+mas install 408981434    # iMovie
+
+############
+# SETTINGS #
+############
 
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "GLaDOS"
