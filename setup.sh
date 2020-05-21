e22f2ee170d0dc35fa38266f962a9d656520a3ea
Joseph Bateh <josephbateh@gmail.com>
2020-05-21 01:57:18 +0000

    Xcode tools

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# XCode Tools
+xcode-select —install
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
