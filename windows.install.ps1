3889e431efab172df93c057fb4b0c2d80fd0f643
Joseph Bateh <github@josephbateh.com>
2022-12-05 19:24:22 -0500

    2022-12-05

--- windows.install.ps1
+++ windows.install.ps1
@@ -2,22 +2,36 @@
 # Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
 # Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
 
+$ErrorActionPreference = "Continue"
+
 Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage *windowscamera* | Remove-AppxPackage
-Get-AppxPackage *officehub* | Remove-AppxPackage
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
-Get-AppxPackage *getstarted* | Remove-AppxPackage
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-Get-AppxPackage *windowsmaps* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *3dviewer* | Remove-AppxPackage
 Get-AppxPackage *bingfinance* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
 Get-AppxPackage *bingnews* | Remove-AppxPackage
-Get-AppxPackage *onenote* | Remove-AppxPackage
 Get-AppxPackage *bingsports* | Remove-AppxPackage
-Get-AppxPackage *soundrecorder* | Remove-AppxPackage
 Get-AppxPackage *bingweather* | Remove-AppxPackage
+Get-AppxPackage *CandyCrush* | Remove-AppxPackage
+Get-AppxPackage *CandyCrushSodaSaga* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *OneNote* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *SkypeApp* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *Windows.XboxMusic* | Remove-AppxPackage
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage *XboxApp* | Remove-AppxPackage
+Get-AppxPackage *XboxGameCallableUI* | Remove-AppxPackage
+Get-AppxPackage *XboxIdentityProvider* | Remove-AppxPackage
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *ZuneVideo* | Remove-AppxPackage
 
 choco install virtualbox
 choco install vlc
