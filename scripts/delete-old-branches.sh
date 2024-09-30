7f4458051535ad51c5f6a03bf7f87601bfc4afb0
Joseph Bateh <github@josephbateh.com>
2024-09-30 11:51:31 -0400

    2024-09-30

---
+++ scripts/delete-old-branches.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script deletes stale local branches.
+
+STALE_DAY_THRESHOLD=30
+RESERVED_PREFIX="saved/"
+
+# Calculate the timestamp for the threshold
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  threshold_timestamp=$(date -v-"$STALE_DAY_THRESHOLD"d +%s)
+else
+  threshold_timestamp=$(date --date="$STALE_DAY_THRESHOLD days ago" +%s)
+fi
+
+# Iterate over each local branch
+git for-each-ref --format='%(refname:short) %(committerdate:unix)' refs/heads/ | while read branch date; do
+  # Skip branches that start with the reserved prefix
+    if [[ $branch == $RESERVED_PREFIX* ]]; then
+      continue
+    fi
+
+  # Check if the branch is older than the threshold
+  if [ $date -eq $threshold_timestamp ]; then
+    git branch -d "$branch"
+  fi
+done
